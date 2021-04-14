6cec5ca37ad9558e180a645f11472e061b81ebd9
Josue David Coreas Paiz <josuepaiz487@gmail.com>
2021-04-14 15:58:02 -0600

    Frontend of UI client

---
+++ WebApp/Configuration/ConfigureCoreServices.cs
@@ -0,0 +1,31 @@
+﻿using ApplicationCore;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Services;
+using Infraestructure.Data;
+using Infraestructure.Loggin;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp.Configuration
+{
+    public static class ConfigureCoreServices
+    {
+        public static IServiceCollection AddCoreServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(MyRepository<>));
+            //services.AddScoped(typeof(IReadRepository<>), typeof(CachedRepository<>));
+            //services.AddScoped(typeof(MyRepository<>));
+
+            //Services
+            //services.AddScoped<IBasketService, BasketService>();
+
+            //Repositories
+            //services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.AddSingleton<IUriComposer>(new UriComposer(configuration.Get<ApplicationSettings>()));
+            services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
+
+            return services;
+        }
+    }
+}
